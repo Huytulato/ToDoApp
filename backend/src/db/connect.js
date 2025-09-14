55d6a07666b57846f12fef22eb2590412c932125
Huytulato <165508189+Huytulato@users.noreply.github.com>
2025-09-14 23:00:48 +0700

    feat: deploy web

--- backend/src/db/connect.js
+++ backend/src/db/connect.js
@@ -3,7 +3,15 @@ import mongoose from "mongoose";
 const connect = async () => {
   try {
     console.log("Attempting to connect to database.....");
-    await mongoose.connect(process.env.MONGO_URI, {});
+    
+    if (!process.env.MONGO_URI) {
+      throw new Error("MONGO_URI environment variable is not defined");
+    }
+    
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to database.....");
   } catch (error) {
     console.log("Failed to connect to database.....", error.message);
